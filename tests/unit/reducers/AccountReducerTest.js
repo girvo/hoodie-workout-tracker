36712a2c71b5faca342a8219d6e70e3dbc88303e
Josh Girvin <josh@jgirvin.com>
2016-03-05 12:36:32 +1000

    Successful sign-up test is added

--- tests/unit/reducers/AccountReducerTest.js
+++ tests/unit/reducers/AccountReducerTest.js
@@ -19,16 +19,45 @@ function createTestState(newState = {}) {
     }).merge(newState, {deep: true})
 }
 
+function createAction(type, payload = null, error = null) {
+    let action = { type }
+
+    if (payload !== null) {
+        action.payload = payload
+    }
+
+    if (error !== null) {
+        action.error = error
+    }
+
+    return action
+}
+
 test.only('reducers/AccountReducer', t => {
-    t.plan(1)
+    t.plan(2)
 
     t.test('SIGNUP_REQUEST', t => {
         const state = createTestState()
-        const action = { type: types.SIGNUP_REQUEST }
+        const action = createAction(types.SIGNUP_REQUEST)
         const result = reducer(state, action)
 
         t.equal(result.signup.loading, true,
             'optimisitically sets the loading value')
+        t.end()
+    })
+
+    t.test('SIGNUP_SUCCESS', t => {
+        const initialState = { signup: { loading: true } }
+        const state = createTestState(initialState)
+        const action = createAction(types.SIGNUP_SUCCESS, { hello: 'world' })
+        const result = reducer(state, action)
+
+        t.equal(result.signup.loading, false,
+            'signup loading is set to false')
+        t.equal(result.signup.user.hello, 'world',
+            'signup user is set to payload')
+        t.equal(result.signup.error, null,
+            'successful signup sets error to null')
         t.end()
     })
 })
